b59637af3f1526d5361554bbf519091b51487725
ryswm <rswoodworth29@gmail.com>
2019-02-12 13:45:02 -0500

    Fixing sim sickness when teleporting

--- Lab3/Assets/Scripts/ControllerInput.cs
+++ Lab3/Assets/Scripts/ControllerInput.cs
@@ -7,15 +7,19 @@ public class ControllerInput : MonoBehaviour {
     public GameObject laser;    // Laser model
     private GameObject copy;    //Laser copy
     private LineRenderer beam;  //Laser renderer
+
     public GameObject telePointer;  //Teleport point model
     private GameObject copyPoint;   //Teleport point copy
+
     private GameObject player;         //Player
-    public GameObject cam;
+    public GameObject cam;              // Camera
 
     RaycastHit point;
 
 
     private bool canTele;                   //If teleport function should be called
+
+    private FadeController fadeEffect;
 
     private SteamVR_TrackedObject trackedObj;
 
@@ -49,7 +53,9 @@ public class ControllerInput : MonoBehaviour {
         beam.enabled = false;
 
         player = GameObject.Find("[CameraRig]");
-       // cam = GameObject.Find("Camera (eye)");
+        cam = GameObject.Find("Camera (eye)");
+
+        fadeEffect = GameObject.Find("FadeEffect").GetComponent<FadeController>();
     }
 	
 	// Update is called once per frame
@@ -66,45 +72,51 @@ public class ControllerInput : MonoBehaviour {
 
             beam.SetPosition(0, line.origin);
 
-            if (Physics.Raycast(line, out point, 100))      //If raycast hits
+            if (Physics.Raycast(line, out point, 100))              //If raycast hits
             {
                 beam.SetPosition(1, point.point);
-                if (point.transform.tag == "Floor")     //If raycast hits floor
+                if (point.transform.tag == "Floor")//If raycast hits floor
                 {
                     canTele = true;
 
                     copyPoint.transform.position = point.point;
                     copyPoint.SetActive(true);
                 }
-                else                                          //If not hitting floor
+                else                             //If not hitting floor
                 {
                     canTele = false;
                     copyPoint.SetActive(false);
                 }
             }
-            else //If raycast does not hit
+            else                                                  //If raycast does not hit
             {
                 beam.SetPosition(1, line.GetPoint(100));
             }
-        } else {                                            //If button not pushed
+        } else {                                                                    //If button not pushed
 
             beam.enabled = false;
             copyPoint.SetActive(false);
 
             if(canTele)
             {
+                
                 Teleport();
+                
             }
         }
 	}
 
     void Teleport()
     {
-        canTele = false;
-        Vector3 diff = player.transform.position - cam.transform.position;
-        diff.y = 0;
+        fadeEffect.FadeEffect();
 
-        player.transform.position = point.point + diff;
+        canTele = false;                //Resetting Variable
+        Vector3 diff = player.transform.position - cam.transform.position;      //Keeping y position
+        diff.y = 0;
+        player.transform.position = point.point + diff;                     // Moving
+
+        fadeEffect.OnFadeReturn();
     }
+
 
 }
---
+++ Lab3/Assets/Scripts/FadeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeController : MonoBehaviour {
+
+	public Animator animate;
+
+	public void FadeEffect(){
+		animate.SetTrigger("FadeOut");
+	}
+
+	public void OnFadeReturn(){
+		animate.SetTrigger("FadeIn");
+	}
+}
