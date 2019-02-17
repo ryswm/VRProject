d395d50d1c04136503f5a06b0d82c84413f53930
andy <andy.llactahuamani@ryerson.ca>
2019-02-17 12:31:24 -0500

    Small changes to control box size

---
+++ Lab4/Assets/Scripts/Interactable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class Interactable : MonoBehaviour {
+
+    public bool Stealable = true;
+    public WandController attachedControl { get; private set; };
+}
