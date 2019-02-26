4f628e8748e0028aae8a4ad9ed36321953c14a5e
ryswm <rswoodworth29@gmail.com>
2019-02-25 22:04:05 -0500

    Implemented VRTK in order to use simulator at home; no vrtk actions used

--- Lab4/Assets/Scripts/HandController.cs
+++ Lab4/Assets/Scripts/HandController.cs
@@ -1,30 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRTK;
 
 public class HandController : MonoBehaviour {
 
     /*
      *  Controller Setup
      */
+    /* 
     private SteamVR_TrackedObject trackedObject;
     private SteamVR_Controller.Device Controller { get { return SteamVR_Controller.Input((int)trackedObject.index); } }
-    private void Awake() { trackedObject = GetComponent<SteamVR_TrackedObject>(); }
+    private void Awake() { trackedObject = GetComponent<SteamVR_TrackedObject>(); }*/
 
     //Setup
     private LineRenderer laser;
     private RaycastHit hitPoint;
     private GameObject selectedObj;
     private Material[] objMats;
+
+    [Header("Highlight Material:")]
     public Material highlight;
 
+    private bool trigPress;
+
     private void Start() {
-        laser = this.GetComponent<LineRenderer>();
+        if (GetComponent<VRTK_ControllerEvents>() == null)
+            {
+                VRTK_Logger.Error(VRTK_Logger.GetCommonMessage(VRTK_Logger.CommonMessageKeys.REQUIRED_COMPONENT_MISSING_FROM_GAMEOBJECT, "VRTK_ControllerEvents_ListenerExample", "VRTK_ControllerEvents", "the same"));
+                return;
+            }
+
+        //Setup controller event listeners
+        GetComponent<VRTK_ControllerEvents>().TriggerPressed += new ControllerInteractionEventHandler(DoTriggerPressed);
+        GetComponent<VRTK_ControllerEvents>().TriggerReleased += new ControllerInteractionEventHandler(DoTriggerReleased);
+
+        gameObject.AddComponent<LineRenderer>();
+        laser = GetComponent<LineRenderer>();
+        laser.startWidth = 0.01f;
+        laser.endWidth = 0.01f;
+
         laser.enabled = false;
     }
 
     void Update() {
-        if (Controller.GetPress(SteamVR_Controller.ButtonMask.Grip)) {
+        if (trigPress) {
+            Debug.Log("pressed");
             laser.enabled = true;
 
             Ray cast = new Ray(this.transform.position, this.transform.forward);
@@ -37,13 +58,13 @@ public class HandController : MonoBehaviour {
                 if(selectedObj.tag == "Selectable") {
                     SetHighlight(selectedObj);
                 } else {
-                    RemoveHighlight(selectedObj);
+                    if(selectedObj) RemoveHighlight(selectedObj);
                     selectedObj = null;
                 }
             }
             else {
                 laser.SetPosition(1, cast.GetPoint(100));
-                RemoveHighlight(selectedObj);
+                if(selectedObj) RemoveHighlight(selectedObj);
                 selectedObj = null;
             }
         }
@@ -61,7 +82,30 @@ public class HandController : MonoBehaviour {
 
     void RemoveHighlight(GameObject selectedObj) {
         objMats = selectedObj.GetComponent<Renderer>().materials;
-        objMats[1] = null;
+        if(objMats.Length > 1) objMats[1] = null;
         selectedObj.GetComponent<Renderer>().materials = objMats;
     }
+
+
+
+
+
+// ******************************   VRTK Functions  ****************************** \\
+    private void DebugLogger(uint index, string button, string action, ControllerInteractionEventArgs e)
+        {
+            VRTK_Logger.Info("Controller on index '" + index + "' " + button + " has been " + action
+                    + " with a pressure of " + e.buttonPressure + " / trackpad axis at: " + e.touchpadAxis + " (" + e.touchpadAngle + " degrees)");
+        }
+
+        private void DoTriggerPressed(object sender, ControllerInteractionEventArgs e)
+        {
+            DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "TRIGGER", "pressed", e);
+            trigPress = true;
+        }
+
+        private void DoTriggerReleased(object sender, ControllerInteractionEventArgs e)
+        {
+            DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "TRIGGER", "released", e);
+            trigPress = false;
+        }
 }
