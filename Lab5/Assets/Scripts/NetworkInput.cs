35104fcf7129a0fb713b54a67b46bf25ae3a4281
ryswm <rswoodworth29@gmail.com>
2019-03-03 15:17:35 -0500

    Adding Lab5 Skeleton

---
+++ Lab5/Assets/Scripts/NetworkInput.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+//This object is added to the controller network avatars when the program is running as a client.
+[RequireComponent(typeof(Rigidbody), typeof(FixedJoint))]
+public class NetworkInput : MonoBehaviour {
+    
+    public SteamVR_TrackedObject trackedObject;
+    public NetworkedCameraRig player;
+    GameObject model;
+    
+    private void Start()
+    {
+        // Consider why our models are neither meshRenderers on the network avatars, nor child objects of them.
+        // How come we have to use joints? 
+
+        model = transform.parent.Find(this.name + "Model").gameObject;
+    }
+
+    void Update () {
+        if (trackedObject)
+        {
+            // disable hands if not tracked.
+            model.SetActive(trackedObject.isValid);
+            if (trackedObject.isValid) {
+                // have the net avatars track the steamvr tracked-objects
+                transform.position = trackedObject.transform.position;
+                transform.rotation = trackedObject.transform.rotation;
+
+                // Send input events to the NetworkedCameraRig instance. (This is our local player)
+                var input = SteamVR_Controller.Input((int)trackedObject.index);
+                if (input.GetPressDown(Valve.VR.EVRButtonId.k_EButton_ApplicationMenu))
+                {
+                    // Why does this method have to live in the NetworkedCameraRig class?
+                    player.CmdCreateSphere(transform.position + transform.forward, transform.rotation);
+                }
+            } 
+        }
+    }
+
+
+
+    
+}
---
+++ Lab5/Assets/Scripts/NetworkedCameraRig.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class NetworkedCameraRig : NetworkBehaviour {
+
+    // See ColorSync.cs for more info on SyncVars
+    [SyncVar(hook = "OnUserNameChange")]
+    public string UserName;
+    // Scale of the camerarig. This is preferable to editing the CameraRig prefab.
+    public float scaleFactor;
+    //Prefab references
+    public GameObject CameraRigPrefab;
+    // The sphere prefab needs to be registered in the networkManager.
+    public GameObject spherePrefab;
+    //Instance references.
+    GameObject CameraRig;
+    Transform NetHead;
+    Transform TrackedHead;
+    
+
+    void Start () {
+
+        NetHead = transform.Find("Head");
+        
+        // This line is essential:
+        if (isLocalPlayer) {
+
+            //From the login-screen.
+            UserName = PlayerPrefs.GetString("PLAYER_USERNAME");
+            CmdRegisterUserName(UserName);
+
+            CameraRig = Instantiate(CameraRigPrefab);
+
+            //Our starting position is dictated by the networkManager's StartPositions.
+            //These can be placed in the scene (Empty objects) and given a NetworkStartPosition Component
+            //They will be automatically registered.
+            CameraRig.transform.position = transform.position;
+            CameraRig.transform.rotation = transform.rotation;
+            CameraRig.transform.localScale = Vector3.one * scaleFactor;
+            
+            // Add the NetworkInput component to the controller avatars. Remember that this only happens on a local player.
+            var leftNetInput = transform.Find("LeftController").gameObject.AddComponent<NetworkInput>();
+            var rightNetInput = transform.Find("RightController").gameObject.AddComponent<NetworkInput>();
+
+            // Setting References.
+            var controllerManager = CameraRig.GetComponent<SteamVR_ControllerManager>();
+            leftNetInput.trackedObject = controllerManager.left.GetComponent<SteamVR_TrackedObject>();
+            rightNetInput.trackedObject = controllerManager.right.GetComponent<SteamVR_TrackedObject>();
+            leftNetInput.player = this;
+            rightNetInput.player = this;
+            var svrCamera = CameraRig.transform.GetComponentInChildren<SteamVR_Camera>();
+            TrackedHead = svrCamera.transform;
+
+            // Small hack to avoid seeing the username text (You could also put it above the player..)
+            svrCamera.camera.nearClipPlane = 0.26f;
+        }
+        else {
+            // When we start a client, It recieves the initial state of the syncvars,
+            // but it doesn't call the hook methods. We need to do this ourselves.
+            OnUserNameChange(UserName);
+        }
+    }
+
+    
+    [Command]
+    internal void CmdCreateSphere(Vector3 pos, Quaternion rot)
+    {
+        //Instantiate on the sever
+        GameObject obj = Instantiate(spherePrefab, pos, rot);
+        //Instantiate on the client
+        NetworkServer.Spawn(obj);
+    }
+
+    void Update () {
+        // We do the head Avatar tracking here.
+        if (isLocalPlayer) {
+            NetHead.position = TrackedHead.position;
+            NetHead.rotation = TrackedHead.rotation;
+        }
+    }
+
+    //Syncvar Callback for user's name
+    void OnUserNameChange(string newName)
+    {
+        var model = transform.Find("HeadModel");
+        var tm = model.GetComponentInChildren<Text>();
+        tm.text = newName;
+        UserName = newName;
+    }
+
+    // Sends a message from the local player so that the server can then propagate to other clients.
+    [Command]
+    void CmdRegisterUserName(string username) {
+        this.UserName = username;
+    }
+}
