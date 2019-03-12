8305cfe14f0e9160a5feb16e7670d32bf7e6db9f
rwoodwor <rwoodwor@scs.ryerson.ca>
2019-03-12 16:34:18 -0400

    Testing scene switching

---
+++ Project/Assets/SceneManager_Lobby.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class SceneManager_Lobby : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col){
+		Debug.Log("Hu");
+		if(col.CompareTag("Player")){
+			SceneManager.LoadScene("Music");
+		}
+	}
+}
