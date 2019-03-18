d9a0115933ec3e3aad58ebcec17a83fd2527fa67
Tomi <tomi.adegbite11@gmail.com>
2019-03-17 23:47:04 -0400

    Worked on basic audio visualizer

---
+++ Project/Assets/Scripts/MusicScene/AudioPeer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (AudioSource))]
+public class AudioPeer : MonoBehaviour {
+
+    private static int numOfBars = 512;
+
+    AudioSource _audioSource;
+    public static float[] _samples = new float[numOfBars];
+	// Use this for initialization
+	void Start () {
+        _audioSource = GetComponent<AudioSource>();
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        getSpecturmAudioSource();
+
+    }
+
+    void getSpecturmAudioSource()
+    {
+        _audioSource.GetSpectrumData(_samples, 0,FFTWindow.Blackman);
+    }
+}
---
+++ Project/Assets/Scripts/MusicScene/StartVisualizers.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartVisualizers : MonoBehaviour {
+    private static int numOfBars = 512;
+    public GameObject barPrefab;
+    GameObject[] visualBar = new GameObject[numOfBars];
+    public float maxScale = 100000;
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < numOfBars; i++)
+        {
+            GameObject barInstance = (GameObject)Instantiate(barPrefab);
+            barInstance.transform.position = this.transform.position;
+            barInstance.transform.parent = this.transform;
+            barInstance.name = "Bar" + i;
+            this.transform.eulerAngles = new Vector3(0, -0.703125f * i, 0);
+            barInstance.transform.position = new Vector3(0, 10, 45);
+            visualBar[i] = barInstance;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < numOfBars; i++)
+        {
+            if (visualBar[i] != null)
+            {
+                visualBar[i].transform.localScale = new Vector3( 0.25f , (AudioPeer._samples[i] * maxScale) +10,0.25f  );
+                
+            }
+        }
+    }
+}
