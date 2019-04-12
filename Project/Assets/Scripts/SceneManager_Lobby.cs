1e297e8eccd795c91511849f17aea5b1959ba969
Ryan W <rswoodworth29@gmail.com>
2019-04-12 10:41:43 -0400

    Fixed scene changer

--- Project/Assets/Scripts/SceneManager_Lobby.cs
+++ Project/Assets/Scripts/SceneManager_Lobby.cs
@@ -8,8 +8,8 @@ public class SceneManager_Lobby : MonoBehaviour {
 
 	void OnTriggerEnter(Collider col){
 		if(col.CompareTag("Player")){
-			if(this.transform.CompareTag("LiquidTele")){
-				SceneManager.LoadScene("Liquid");
+			if(this.transform.CompareTag("Manipulate")){
+				SceneManager.LoadScene("Manipulate");
 			} else if(this.transform.CompareTag("MusicTele")){
 				SceneManager.LoadScene ("Music");
 		}
